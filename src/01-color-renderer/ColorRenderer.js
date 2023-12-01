ef401550de64b7f842b13fea9b9f412204ab55fb
Elliot Berdy <57967621+elliotberdy@users.noreply.github.com>
2023-12-01 00:12:22 +0000

    testing

--- src/01-color-renderer/ColorRenderer.js
+++ src/01-color-renderer/ColorRenderer.js
@@ -19,9 +19,8 @@ const colors = [{
 
 export default function ColorRenderer () {
   return (
-    <h2>
-      Use the Color component to render each
-       item in the colors array on the page!
-    </h2>
-  )
-}
+    {colors.map((hex, name) => (
+      <Color hex={hex} name={name} />
+    ))}
+  );
+};
