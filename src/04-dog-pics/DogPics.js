e397c609a9cc22c4c3f85c8d747d73a9bc4790f5
Elliot Berdy <57967621+elliotberdy@users.noreply.github.com>
2023-12-01 00:51:36 +0000

    01_04_done

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,27 @@
-export default function DogPics () {
+ // have a state that maintains what the dog image is
+ // when button is clicked get new dog image and update the state
+
+import { useEffect, useState } from "react"
+
+const getDogPic = async () => {
+  const response = await fetch('https://dog.ceo/api/breeds/image/random')
+  const dog = await response.json()
+  return dog.message
+}
+ 
+ export default function DogPics () {
   // API: https://dog.ceo/dog-api/
+
+  const [dogPic, setDogPic] = useState('null')
+
+  useEffect(() => {
+    getDogPic().then(pic => setDogPic(pic))
+  }, [])
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={dogPic} alt="" />
+      <button onClick={async e => setDogPic(await getDogPic())}>🐶</button>
     </div>
   )
 }
